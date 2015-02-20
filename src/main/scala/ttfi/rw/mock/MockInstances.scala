403da62b385c0c5ad24cecc86f35b0563ce76c0a
Guillaume Massé <guillaume.masse@mediative.com>
2015-02-20 09:59:35 -0500

    MPN-619 Modularization - implicit initialization trait sequencing revert

--- src/main/scala/ttfi/rw/mock/MockInstances.scala
+++ src/main/scala/ttfi/rw/mock/MockInstances.scala
@@ -1,8 +1,10 @@
 package ttfi.rw
 package mock
 
-trait MockInstances extends MockMonadInstances {
+trait MockInstances {
   import MockAdt._
+
+  import scalaz.{ State, Monad }
 
   import scalaz.State
   import scalaz.syntax.std.option._
@@ -27,5 +29,22 @@ trait MockInstances extends MockMonadInstances {
           val ((_, fs), content) = mc(s)
           ((Queue(Write(path, content)), fs + (path -> content)), ())
         })
+    }
+
+  // FIXME: check monad's laws with scalacheck
+  implicit val mockMonad: Monad[Mock] =
+    new Monad[Mock] {
+      def point[A](a: => A): Mock[A] = State((_, a))
+      override def map[A, B](m: Mock[A])(f: A => B): Mock[B] = State { s =>
+        val (s1, a) = m(s)
+        (s1, f(a))
+      }
+      def bind[A, B](m: Mock[A])(f: A => Mock[B]): Mock[B] = join2(m.map(f))
+      def join2[A](m: Mock[Mock[A]]): Mock[A] = State { s =>
+        val (s1, m1) = m(s)
+        val (ops1, fs1) = s1
+        val ((ops2, fs2), a) = m1(s1)
+        ((ops1 ++ ops2, fs2), a)
+      }
     }
 }
