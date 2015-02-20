465048ec5a451993c18b15c6f518773a700c06b5
Suhail Shergill <Suhail.Shergill@mediative.com>
2015-02-20 04:48:55 -0500

    MPN-619 Modularization - implicit initialization with objects
    using implicit val's was the cause of NPE observed earlier.
    replacing those with implicit object side-steps the
    initialization issues. as such, we also side-step the issue of needing
    to define a specific ordering (in order to address NPE issue).
    
    'sbt test' now works as expected with no errors

--- src/main/scala/ttfi/rw/mock/MockInstances.scala
+++ src/main/scala/ttfi/rw/mock/MockInstances.scala
@@ -10,41 +10,39 @@ trait MockInstances {
   import scalaz.syntax.std.option._
   import scala.collection.immutable.Queue
 
-  implicit val mockRWSym: RWSym[Mock, MockPath, MockContent] =
-    new RWSym[Mock, MockPath, MockContent] {
-      def delete: Mock[MockPath] => Mock[Boolean] = _.mapK {
-        case ((_, fs), path) =>
-          val deleted = fs.contains(path)
-          ((Queue(Delete(path, deleted)), fs - path), deleted)
-      }
-
-      def read: Mock[MockPath] => Mock[MockContent] = _.mapK {
-        case ((_, fs), path) =>
-          val content = fs.get(path) | MockContent("not found") // FIXME: not found
-          ((Queue(Read(path, content)), fs), content)
-      }
-
-      def write: Mock[MockPath] => Mock[MockContent] => Mock[Unit] = mp => mc =>
-        mp.flatMap(path => State { s =>
-          val ((_, fs), content) = mc(s)
-          ((Queue(Write(path, content)), fs + (path -> content)), ())
-        })
+  implicit object mockRWSym extends RWSym[Mock, MockPath, MockContent] {
+    def delete: Mock[MockPath] => Mock[Boolean] = _.mapK {
+      case ((_, fs), path) =>
+        val deleted = fs.contains(path)
+        ((Queue(Delete(path, deleted)), fs - path), deleted)
     }
+
+    def read: Mock[MockPath] => Mock[MockContent] = _.mapK {
+      case ((_, fs), path) =>
+        val content = fs.get(path) | MockContent("not found") // FIXME: not found
+        ((Queue(Read(path, content)), fs), content)
+    }
+
+    def write: Mock[MockPath] => Mock[MockContent] => Mock[Unit] = mp => mc =>
+      mp.flatMap(path => State { s =>
+        val ((_, fs), content) = mc(s)
+        ((Queue(Write(path, content)), fs + (path -> content)), ())
+      })
+  }
 
   // FIXME: check monad's laws with scalacheck
-  implicit val mockMonad: Monad[Mock] =
-    new Monad[Mock] {
-      def point[A](a: => A): Mock[A] = State((_, a))
-      override def map[A, B](m: Mock[A])(f: A => B): Mock[B] = State { s =>
-        val (s1, a) = m(s)
-        (s1, f(a))
-      }
-      def bind[A, B](m: Mock[A])(f: A => Mock[B]): Mock[B] = join2(m.map(f))
-      def join2[A](m: Mock[Mock[A]]): Mock[A] = State { s =>
-        val (s1, m1) = m(s)
-        val (ops1, fs1) = s1
-        val ((ops2, fs2), a) = m1(s1)
-        ((ops1 ++ ops2, fs2), a)
-      }
+  implicit object mockMonad extends Monad[Mock] {
+    def point[A](a: => A): Mock[A] = State((_, a))
+    override def map[A, B](m: Mock[A])(f: A => B): Mock[B] = State { s =>
+      val (s1, a) = m(s)
+      (s1, f(a))
     }
+    def bind[A, B](m: Mock[A])(f: A => Mock[B]): Mock[B] = join2(m.map(f))
+    def join2[A](m: Mock[Mock[A]]): Mock[A] = State { s =>
+      val (s1, m1) = m(s)
+      val (ops1, fs1) = s1
+      val ((ops2, fs2), a) = m1(s1)
+      ((ops1 ++ ops2, fs2), a)
+    }
+  }
 }
--- src/main/scala/ttfi/rw/nio/NioInstances.scala
+++ src/main/scala/ttfi/rw/nio/NioInstances.scala
@@ -10,25 +10,24 @@ trait NioInstances {
   import java.nio.file._
   import java.nio.charset._
 
-  implicit def evalSym: RWSym[IO, Path, String] =
-    new RWSym[IO, Path, String] {
-      def read = loc => {
-        loc.map { p =>
-          import scala.collection.JavaConverters._
-          Files.readAllLines(p).asScala.mkString(System.lineSeparator)
-        }
+  implicit object evalRWSym extends RWSym[IO, Path, String] {
+    def read = loc => {
+      loc.map { p =>
+        import scala.collection.JavaConverters._
+        Files.readAllLines(p).asScala.mkString(System.lineSeparator)
+      }
+    }
+
+    def write = loc => content =>
+      for {
+        l <- loc
+        c <- content
+      } yield {
+        Files.write(l, c.getBytes(Charset.forName("UTF-8")))
+        ()
       }
 
-      def write = loc => content =>
-        for {
-          l <- loc
-          c <- content
-        } yield {
-          Files.write(l, c.getBytes(Charset.forName("UTF-8")))
-          ()
-        }
-
-      def delete = loc =>
-        loc.map(l => Files.deleteIfExists(l))
-    }
+    def delete = loc =>
+      loc.map(l => Files.deleteIfExists(l))
+  }
 }
